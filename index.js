ee61f0a3687a72908a9a5d4c3bbad77126224ad9
Swapnil Patil <swapnilpatil@Swapnils-MacBook-Pro-2.local>
2020-05-06 18:15:52 -0700

    /beforeclientsidereorg/

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ if (port == null || port == "") {
 /**
  * This is an example of a basic node.js script that performs
  * the Authorization Code oAuth2 flow to authenticate against
- * the Spotify Accounts
+ * the Spotify Accounts 
  *
  * For more information, read
  * https://developer.spotify.com/web-api/authorization-guide/#authorization_code_flow
